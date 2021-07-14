8a086ff26788e0e740c82692b71fd01296283b97
Geordie King <geordie.king@gmail.com>
2021-07-14 18:25:42 +0100

    added todos array and UL to display

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'The Todo List'
+    title: 'The Todo List',
+    newTodoText: '',
+    todos:[]
   },
   methods: {
     addTodo() {
-      console.log('form submitted')
+      // console.log('form submitted')
+      // console.log(this.newTodoText)
+      this.todos.push({
+        title: this.newTodoText,
+        done: false
+      });
+      this.newTodoText = ''
+      // console.log(this.todos)
     }
   }
-
 });
